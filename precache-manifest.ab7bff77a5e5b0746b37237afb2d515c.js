f57033412ba90f3f576f9ffa6241c8bf147fe1df
Amy Sandoval <amysandoval92@gmail.com>
2022-10-05 11:11:54 -0700

    Updates

---
+++ precache-manifest.ab7bff77a5e5b0746b37237afb2d515c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d6dfbdb9a3b332ea7a2",
+    "url": "/ctd-react-albatross/static/css/main.9e4c4534.chunk.css"
+  },
+  {
+    "revision": "6d6dfbdb9a3b332ea7a2",
+    "url": "/ctd-react-albatross/static/js/main.84a1ea3d.chunk.js"
+  },
+  {
+    "revision": "c49c635e0339a9123318",
+    "url": "/ctd-react-albatross/static/js/runtime~main.1896e380.js"
+  },
+  {
+    "revision": "a9785474a0786e0e2bd9",
+    "url": "/ctd-react-albatross/static/js/2.3de40346.chunk.js"
+  },
+  {
+    "revision": "1ce813bd7c54a24c8bf4a226d522947b",
+    "url": "/ctd-react-albatross/static/media/renato-ramos-puma-_3kpyI18Rxk-unsplash.1ce813bd.jpg"
+  },
+  {
+    "revision": "d7161698450222aedd1a086758efe6cd",
+    "url": "/ctd-react-albatross/static/media/renato-ramos-puma-jMJSWuO2RaY-unsplash.d7161698.jpg"
+  },
+  {
+    "revision": "c7d08243ba788ea35894812eac3e79ff",
+    "url": "/ctd-react-albatross/static/media/renato-ramos-puma-lKdLVkZJdPM-unsplash.c7d08243.jpg"
+  },
+  {
+    "revision": "6e0c92a1618bb511fa11c6a4d40018f2",
+    "url": "/ctd-react-albatross/index.html"
+  }
+];
